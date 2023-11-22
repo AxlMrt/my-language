fb1f398d9756f6c591b05d91b8594c1b67025adc
Martin Axel <martin.axel@sfr.fr>
2023-11-22 09:03:52 +0100

    feat: Add more tests cases

--- lexer/tests/lexer_tests.cpp
+++ lexer/tests/lexer_tests.cpp
@@ -1,19 +1,34 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-#include "../src/Lexer.h"
+#include "../src/headers/Lexer.h"
+
+
+TEST_CASE("Lexer tokenizes identifiers correctly")
+{
+  Lexer lexer;
+  Token tokens[50];
+
+  SECTION("Test case: Single identifiers")
+  {
+    lexer.scan("variable", tokens);
+    REQUIRE(tokens[0].type == TokenType::IDENTIFIER);
+  }
+
+  SECTION("Test case: Multiple identifiers")
+  {
+    lexer.scan("variable1 var_2 anotherVar int_var string_var bool_var decimal_var var124", tokens);
+    const TokenType expectedType = TokenType::IDENTIFIER;
+
+    for (int i = 0; i < 8; ++i)
+      REQUIRE(tokens[i].type == expectedType);
+  }
+}
+
 
 TEST_CASE("Lexer can tokenize simple expressions")
 {
   Lexer lexer;
-
-  SECTION("Test case 1: Identifiers")
-  {
-    Token tokens[50];
-    lexer.scan("variable another_variable int_var string_var bool_var decimal_var var124", tokens);
-    for (int i = 0; i < 7; i++)
-      REQUIRE(tokens[i].type == TokenType::IDENTIFIER);
-  }
 
   SECTION("Test case 2: Keywords")
   {
@@ -31,5 +46,30 @@ TEST_CASE("Lexer can tokenize simple expressions")
     lexer.scan("bool", tokens);
     REQUIRE(tokens[0].type == TokenType::KEYWORD_BOOL);
 
+    lexer.scan("dspl", tokens);
+    REQUIRE(tokens[0].type == TokenType::KEYWORD_DSPL);
+  }
+
+  SECTION("Test case 3: Operations")
+  {
+    Token tokens[50];
+    struct Operation
+    {
+      const char *operationString;
+      TokenType operationToken;
+    };
+
+    const Operation operations[] = {
+      { "a = b + c;", TokenType::PLUS },
+      { "a = b - c;", TokenType::MINUS },
+      { "a = b * c;", TokenType::MULTIPLY },
+      { "a = b / c;", TokenType::DIVIDE },
+    };
+
+    for (const Operation &operation : operations)
+    {
+      lexer.scan(operation.operationString, tokens);
+      REQUIRE(tokens[3].type == operation.operationToken);
+    }
   }
 }

0cbdde3db70244287ac48f1522bb9fe683afe50a
Axel Martin <martin.axel@sfr.fr>
2023-12-07 11:08:28 +0100

    Fix: add more types to DisplayHandler and fix VariableHandler

--- parser/src/Parser.cpp
+++ parser/src/Parser.cpp
@@ -1,26 +1,32 @@
 #include "../include/Parser.h"
-#include "../include/VariableHandler.h"
 #include "../include/DisplayHandler.h"
+#include "../include/VariableHandler.h"
 #include <iostream>
 
 using namespace std;
 
-Parser::Parser(Token *tokens, int count)
-    : tokenStream(tokens), tokenCount(count), currentTokenIndex(0) {}
+Parser::Parser(Token *tokens, int count) : tokenStream(tokens), tokenCount(count), currentTokenIndex(0)
+{
+}
 
 void Parser::parse()
 {
-  while (currentTokenIndex < tokenCount)
-  {
-    if (tokenStream[currentTokenIndex].type == TokenType::KEYWORD_DSPL) {
-      DisplayHandler displayHandler;
-      displayHandler.parseDSPL(tokenStream, currentTokenIndex, variables);
-    } else if (tokenStream[currentTokenIndex].type == TokenType::IDENTIFIER) {
-      VariableHandler variableHandler;
-      variableHandler.parseVariableDeclaration(tokenStream, currentTokenIndex, variables);
-    } else {
-      cout << "Erreur : Instruction non reconnue." << endl;
-      ++currentTokenIndex;
+    while (currentTokenIndex < tokenCount)
+    {
+        if (tokenStream[currentTokenIndex].type == TokenType::KEYWORD_DSPL)
+        {
+            DisplayHandler displayHandler;
+            displayHandler.parseDSPL(tokenStream, currentTokenIndex, variables);
+        }
+        else if (tokenStream[currentTokenIndex].type == TokenType::IDENTIFIER)
+        {
+            VariableHandler variableHandler;
+            variableHandler.parseVariableDeclaration(tokenStream, currentTokenIndex, variables);
+        }
+        else
+        {
+            cout << "Erreur : Instruction non reconnue." << endl;
+            ++currentTokenIndex;
+        }
     }
-  }
 }
--- parser/src/VariableHandler.cpp
+++ parser/src/VariableHandler.cpp
@@ -3,65 +3,71 @@
 
 using namespace std;
 
-VariableHandler::VariableHandler() {}
-
+VariableHandler::VariableHandler()
+{
+}
 
 bool VariableHandler::hasVariableType(Token *tokens, int currentIndex)
 {
-  return (tokens[currentIndex].type == TokenType::KEYWORD_INT ||
-          tokens[currentIndex].type == TokenType::KEYWORD_DECIMAL ||
-          tokens[currentIndex].type == TokenType::KEYWORD_BOOL ||
-          tokens[currentIndex].type == TokenType::KEYWORD_STRING);
+    return (tokens[currentIndex].type == TokenType::KEYWORD_INT ||
+            tokens[currentIndex].type == TokenType::KEYWORD_DECIMAL ||
+            tokens[currentIndex].type == TokenType::KEYWORD_BOOL ||
+            tokens[currentIndex].type == TokenType::KEYWORD_STRING);
 }
 
-void VariableHandler::parseVariableDeclaration(Token *tokens, int &currentIndex, unordered_map<const char*, const char*, CharPtrHash, CharPtrEqual> &variables)
+void VariableHandler::parseVariableDeclaration(
+    Token *tokens, int &currentIndex, unordered_map<const char *, const char *, CharPtrHash, CharPtrEqual> &variables)
 {
-  if (tokens[currentIndex].type != TokenType::IDENTIFIER)
-  {
-    cout << "Erreur : Nom de variable attendu." << endl;
-    return;
-  }
+    if (tokens[currentIndex].type != TokenType::IDENTIFIER)
+    {
+        cout << "Erreur : Nom de variable attendu." << endl;
+        return;
+    }
 
-  const char* nom_variable = tokens[currentIndex].lexeme;
-  ++currentIndex;
+    const char *nom_variable = tokens[currentIndex].lexeme;
+    ++currentIndex;
 
-  if (tokens[currentIndex].type != TokenType::COLON)
-  {
-    cout << "Erreur : Symbole ':' attendu." << endl;
-    return;
-  }
+    if (tokens[currentIndex].type != TokenType::COLON)
+    {
+        cout << "Erreur : Symbole ':' attendu." << endl;
+        return;
+    }
 
-  ++currentIndex;
+    ++currentIndex;
 
-  if (!hasVariableType(tokens, currentIndex))
-  {
-    cout << "Erreur : Type de variable attendu." << endl;
-    return;
-  }
-  ++currentIndex;
+    if (!hasVariableType(tokens, currentIndex))
+    {
+        cout << "Erreur : Type de variable attendu." << endl;
+        return;
+    }
 
-  if (tokens[currentIndex].type != TokenType::ASSIGN)
-  {
-    cout << "Erreur : Symbole '=' attendu." << endl;
-    return;
-  }
-  ++currentIndex;
-    
+    ++currentIndex;
+
+    if (tokens[currentIndex].type != TokenType::ASSIGN)
+    {
+        cout << "Erreur : Symbole '=' attendu." << endl;
+        return;
+    }
+
+    ++currentIndex;
+
     // A modifier pour assigner une valeur plus loin dans le code
-  if ((tokens[currentIndex].type != TokenType::INTEGER && tokens[currentIndex].type != TokenType::DECIMAL &&
-        tokens[currentIndex].type != TokenType::STRING && tokens[currentIndex].type != TokenType::BOOLEAN))
-  {
-    cout << "Erreur : Valeur attendue pour la variable." << endl;
-    return;
-  }
-  ++currentIndex;
+    if ((tokens[currentIndex].type != TokenType::INTEGER && tokens[currentIndex].type != TokenType::DECIMAL &&
+         tokens[currentIndex].type != TokenType::STRING && tokens[currentIndex].type != TokenType::BOOLEAN))
+    {
+        cout << "Erreur : Valeur attendue pour la variable." << endl;
+        return;
+    }
 
-  if (tokens[currentIndex].type != TokenType::SEMICOLON)
-  {
-    cout << "Erreur : Symbole ';' attendu." << endl;
-    return;
-  }
+    const char *valeur_variable = tokens[currentIndex].lexeme;
+    ++currentIndex;
 
-  variables[nom_variable] = tokens[currentIndex].lexeme;
-  ++currentIndex;
+    if (tokens[currentIndex].type != TokenType::SEMICOLON)
+    {
+        cout << "Erreur : Symbole ';' attendu." << endl;
+        return;
+    }
+
+    variables[nom_variable] = valeur_variable;
+    ++currentIndex;
 }
